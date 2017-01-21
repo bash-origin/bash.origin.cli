022cf3d028d8c3c0d19fd88cf98983b1ed85fbe8
Christoph Dorn <christoph@christophdorn.com>
2017-01-21 00:17:24 -0800

    initial

---
+++ org.bashorigin-1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash.origin.script
+
+function EXPORTS_fingerprint {
+
+    # TODO: Expand on fingeprint
+    # TODO: Allow for different fingerprint formats
+
+    echo "$(uname)"
+}
